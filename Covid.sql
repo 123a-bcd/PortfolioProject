7bbbfa5ea3744f2ab65e15d32d4f6916ca940b5e
123a-bcd <hagiadong26@gmail.com>
2024-11-28 18:52:14 +0700

    COVID-19 Data Analysis Project.sql
    This project focuses on analyzing and visualizing COVID-19 data to uncover insights regarding infection rates, death rates, and vaccination progress worldwide.

--- Covid.sql
+++ Covid.sql
@@ -1,20 +1,16 @@
+-- Select all columns and sort by specific indices
 select * from Portfolio..CovidDeaths
 order by 3,4
 
 
--- Select * from Portfolio..CovidDeaths
---order by 3,4
-
-
--- Select data that we are going to using
+-- Select specific columns for analysis and sort by location and date
 select location, date, total_cases, new_cases, total_deaths, population
 from Portfolio..CovidDeaths
 where continent is not null
 order by 1,2
 
 
--- Looking at Total Cases vs Total Deaths
--- Show likelihood of dying if you contract Covid in your country
+-- Show the likelihood of dying if you contract COVID-19 in Viet Nam
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from Portfolio..CovidDeaths
 where location = 'Vietnam'
@@ -22,8 +18,7 @@ and continent is not null
 order by 2
 
 
--- Looking at the Total Cases vs Population
--- Show what percentage of population got Covid
+-- Calculate the percentage of population infected with COVID-19
 select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
 from Portfolio..CovidDeaths
 where location = 'Vietnam'
@@ -31,7 +26,7 @@ and continent is not null
 order by 1,2
 
 
--- Looking at country with Highest Infection Rate compared to Population
+-- Find the country with the highest infection rate relative to its population
 select location, population, max(total_cases) as HighesInfectionCount, max((total_cases/population)*100) as PercentPopulationInfected
 from Portfolio..CovidDeaths
 where continent is not null
@@ -39,7 +34,7 @@ group by location, population
 order by PercentPopulationInfected desc
 
 
--- Showing Country with Highest Death Count per Population
+-- Identify the country with the highest number of total deaths
 select location, max(cast(total_deaths as int)) as TotalDeathCount
 from Portfolio..CovidDeaths
 where continent is not null
@@ -47,7 +42,7 @@ group by location
 order by TotalDeathCount desc
 
 
--- Break things down by continent
+-- Summarize total deaths by continent
 select continent, max(cast(total_deaths as int)) as TotalDeathCount
 from Portfolio..CovidDeaths
 where continent is not null
@@ -55,7 +50,7 @@ group by continent
 order by TotalDeathCount desc
 
 
--- Showing continents with the highes death count per population
+-- Calculate the death percentage by continent
 select continent, sum(cast(total_deaths as int))/sum(population)*100 as PercentDeathContinent
 from Portfolio..CovidDeaths
 where continent is not null
@@ -63,7 +58,7 @@ group by continent
 order by PercentDeathContinent desc
 
 
--- Global number
+-- Summarize global cases, deaths, and death percentage
 select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
 from Portfolio..CovidDeaths
 where continent is not null
@@ -71,7 +66,7 @@ where continent is not null
 order by 1,2
 
 
--- Looking at Total Population vs Vaccinations
+-- Calculate cumulative vaccinations by country
 select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 ,sum(cast(cv.new_vaccinations as int)) over (partition by cd.location order by cd.location,
  cd.date) as RollingPeopleVaccinated
@@ -83,7 +78,7 @@ where cd.continent is not null and cd.location = 'Albania'
 order by 2,3
 
 
--- CTE
+-- Use a CTE to calculate rolling vaccinations and vaccination percentage
 with PopvsVac (Continent, Location, Date, Population, New_Vaccination, RollingPeopleVaccinated)
 as 
 (
@@ -101,7 +96,7 @@ select *, (RollingPeopleVaccinated/Population)*100
 from PopvsVac
 
 
--- temp table
+-- Create a temporary table for vaccination data
 create Table #PercentPopulationVaccinated
 (
 Continent nvarchar(255),
@@ -112,7 +107,7 @@ New_vaccinations numeric,
 RollingPeopleVaccinated numeric,
 )
 
-
+-- Insert vaccination data into the temporary table
 insert into #PercentPopulationVaccinated
 select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 ,sum(cast(cv.new_vaccinations as int)) over (partition by cd.location order by cd.location,
@@ -124,11 +119,12 @@ and cd.date = cv.date
 --where cd.continent is not null 
 --order by 2,3
 
+ -- Query the temporary table to calculate the vaccination percentage
 select *, (RollingPeopleVaccinated/Population)*100
 from #PercentPopulationVaccinated
 
 
--- Creating view to store data for later visualizations
+-- Create a view for long-term use in data visualization
 create view PercentPopulationVaccinated as
 select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 ,sum(cast(cv.new_vaccinations as int)) over (partition by cd.location order by cd.location,
